aaacc865a9b164a04276ad8de62c9c5027195269
Oleg <107485701+sal1r@users.noreply.github.com>
2024-08-22 16:47:46 +0300

    Update DragAndDrop.kt

--- app/src/main/java/com/example/draganddrop/DragAndDrop.kt
+++ app/src/main/java/com/example/draganddrop/DragAndDrop.kt
@@ -3,6 +3,7 @@ package com.example.draganddrop
 import android.content.Context
 import android.os.Build
 import android.view.WindowManager
+import androidx.annotation.FloatRange
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.lazy.LazyListState
@@ -22,7 +23,8 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlin.math.abs
+import kotlin.time.DurationUnit
+import kotlin.time.measureTime
 
 /**
  * State of Drag&Drop in LazyList. Creates with rememberLazyListDragAndDropState()
@@ -62,12 +64,18 @@ fun rememberLazyListDragAndDropState(): LazyListDragAndDropState = remember {
  * @param lazyListState the state of current LazyList
  * @param dragAndDropState state of Drag&Drop. Used to get information about dragging
  * @param onItemsOrderChanged callback where you should reorder list used in LazyList
+ * @param autoscrollSpeed speed of autoscroll in px per second
+ * @param startAutoscrollBound start bound of autoscroll,
+ * @param endAutoscrollBound end bound of autoscroll
  */
 @Composable
 fun Modifier.dragAndDrop(
     lazyListState: LazyListState,
     dragAndDropState: LazyListDragAndDropState,
-    onItemsOrderChanged: (lastIndex: Int, newIndex: Int) -> Unit
+    onItemsOrderChanged: (lastIndex: Int, newIndex: Int) -> Unit,
+    @FloatRange(from = 0.0) autoscrollSpeed: Float = 1500f,
+    startAutoscrollBound: Float = 50f,
+    endAutoscrollBound: Float = -50f
 ): Modifier {
     val context = LocalContext.current
     val fps = rememberSaveable {
@@ -83,54 +91,73 @@ fun Modifier.dragAndDrop(
     var itemIndex by state._draggedItemIndex
     var itemOffset by state._draggedItemOffset
 
-    suspend fun updateItemIndex(newPos: Float) {
-        lazyListState.layoutInfo.visibleItemsInfo
-            .minByOrNull { info ->
-                abs(newPos - (info.offset + info.size / 2f))
-            }
-            ?.let {
-                val fvii = lazyListState.firstVisibleItemIndex
-                val fviso = lazyListState.firstVisibleItemScrollOffset
-                val lastIndex = itemIndex!!
-                val newIndex = it.index
-                if (lastIndex != newIndex)
-                    itemOffset = itemOffset?.minus(
-                        (newIndex - lastIndex) * it.size
+    fun updateItemIndex(newPos: Float) {
+        lazyListState.layoutInfo.visibleItemsInfo.let { visibleItemsInfo ->
+            val fvii = lazyListState.firstVisibleItemIndex
+            val fviso = lazyListState.firstVisibleItemScrollOffset
+            val lastIndex = itemIndex!!
+            val draggedItem = visibleItemsInfo[lastIndex - lazyListState.firstVisibleItemIndex]
+            visibleItemsInfo.firstOrNull { info ->
+                newPos.toInt().coerceIn(lazyListState.layoutInfo.viewportStartOffset + 1, lazyListState.layoutInfo.viewportEndOffset - 1) in (info.offset + if (info.index > draggedItem.index) info.size - draggedItem.size else 0)..(info.offset + if (info.index > draggedItem.index) info.size else draggedItem.size)
+            }?.let { closest ->
+                val newIndex = closest.index
+
+                if (lastIndex != newIndex) {
+                    itemOffset = itemOffset?.plus(
+                        (
+                            visibleItemsInfo[newIndex - fvii].size + (
+                                if (newIndex < lastIndex) ((lastIndex - fvii + 1)..<(newIndex - fvii))
+                                else ((newIndex - fvii + 1)..<(lastIndex - fvii))
+                            ).sumOf {
+                                visibleItemsInfo[it].size
+                            }
+                        ) * if (newIndex < lastIndex) 1f else -1f
                     )
+                }
 
                 onItemsOrderChanged(lastIndex, newIndex)
 
                 itemIndex = newIndex
 
-                if (fvii == lastIndex) {
-                    lazyListState.scrollToItem(lastIndex, fviso)
-                } else if (fvii == newIndex) {
-                    lazyListState.scrollToItem(newIndex, fviso)
+                if (fvii == lastIndex || fvii == newIndex) {
+                    coroutineScope.launch {
+                        lazyListState.scrollToItem(fvii, fviso)
+                    }
                 }
             }
+        }
     }
 
     LaunchedEffect(Unit) {
+        var lastFrameRemains = 0f
         while (true) {
-            position?.let Pos@ { pos ->
-                val scrollValue = 1500f / fps *
-                        if (pos > lazyListState.layoutInfo.viewportEndOffset - 50) 1
-                        else if (pos < lazyListState.layoutInfo.viewportStartOffset + 50) -1
-                        else 0
+            measureTime {
+                position?.let Pos@ { pos ->
+                    val scrollValue = autoscrollSpeed / fps *
+                            if (pos > lazyListState.layoutInfo.viewportEndOffset + endAutoscrollBound) 1
+                            else if (pos < lazyListState.layoutInfo.viewportStartOffset + startAutoscrollBound) -1
+                            else 0
 
-                itemOffset?.plus(scrollValue)?.let {
-                    if (itemIndex == 0 && it < 0 ||
-                        itemIndex == lazyListState.layoutInfo.totalItemsCount - 1 && it > 0
-                    ) return@let
+                    itemOffset?.plus(scrollValue)?.let {
+                        itemOffset = if (itemIndex == 0 && it < 0 ||
+                            itemIndex == lazyListState.layoutInfo.totalItemsCount - 1 && it > 0
+                        ) 0f else it
+                    }
 
-                    itemOffset = it
+                    lazyListState.scrollBy(scrollValue)
+
+                    updateItemIndex(pos)
                 }
+            }.let {
+                val lastFrameDuration = it.toDouble(DurationUnit.MILLISECONDS)
 
-                lazyListState.scrollBy(scrollValue)
+                delay((1000 / fps - lastFrameDuration - lastFrameRemains)
+                    .toLong().coerceAtLeast(0))
 
-                updateItemIndex(pos)
+                lastFrameRemains =
+                    if (lastFrameDuration > 1000 / fps) (lastFrameDuration - 1000 / fps).toFloat()
+                    else 0f
             }
-            delay((1000 / fps).toLong())
         }
     }
 
@@ -157,25 +184,23 @@ fun Modifier.dragAndDrop(
             },
             onDrag = { change, dragAmount ->
                 change.consume()
-                coroutineScope.launch {
-                    position = position?.plus(dragAmount.y)
+                position = position?.plus(dragAmount.y)
 
-                    itemOffset?.let {
-                        val newOffset = it + dragAmount.y
+                itemOffset?.let {
+                    val newOffset = it + dragAmount.y
 
-                        if (itemIndex == 0 && newOffset < 0 ||
-                            itemIndex == (lazyListState.layoutInfo.totalItemsCount - 1) && newOffset > 0
-                        ) {
-                            itemOffset = 0f
-                            return@launch
-                        }
-
-                        itemOffset = newOffset
+                    if (itemIndex == 0 && newOffset < 0 ||
+                        itemIndex == (lazyListState.layoutInfo.totalItemsCount - 1) && newOffset > 0
+                    ) {
+                        itemOffset = 0f
+                        return@detectDragGesturesAfterLongPress
                     }
 
-                    position?.let { pos ->
-                        updateItemIndex(pos)
-                    }
+                    itemOffset = newOffset
+                }
+
+                position?.let { pos ->
+                    updateItemIndex(pos)
                 }
             }
         )
